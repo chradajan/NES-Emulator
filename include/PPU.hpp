dc8a5e8db4e29ca7b50efb20de8f49e337088efd
chradajan <chradajan@gmail.com>
2022-08-01 11:45:43 -0700

    Implement PPU memory mapped register reading/writing

--- include/PPU.hpp
+++ include/PPU.hpp
@@ -1,8 +1,40 @@
 #ifndef PPU_HPP
 #define PPU_HPP
 
+#include <array>
 #include <cstdint>
 #include <memory>
+
+// PPUCTRL $2000
+
+constexpr uint8_t BASE_NT_ADDRESS_MASK = 0x03;
+constexpr uint8_t INCREMENT_VRAM_MASK = 0x04;
+constexpr uint8_t SPRITE_PT_ADDRESS_MASK = 0x08;
+constexpr uint8_t BACKGROUND_PT_ADDRESS_MASK = 0x10;
+constexpr uint8_t SPRITE_SIZE_MASK = 0x20;
+constexpr uint8_t MASTER_SLAVE_SELECT_MASK = 0x40;
+constexpr uint8_t GENERATE_NMI_MASK = 0x80;
+
+// PPUMASK $2001
+
+constexpr uint8_t GREYSCALE_MASK = 0x01;
+constexpr uint8_t SHOW_LEFT_BACKGROUND_MASK = 0x02;
+constexpr uint8_t SHOW_LEFT_SPRITE_MASK = 0x04;
+constexpr uint8_t SHOW_BACKGROUND_MASK = 0x08;
+constexpr uint8_t SHOW_SPITES_MASK = 0x10;
+constexpr uint8_t EMPHASIZE_RED_MASK = 0x20;
+constexpr uint8_t EMPHASIZE_GREEN_MASK = 0x40;
+constexpr uint8_t EMPHASIZE_BLUE_MASK = 0x80;
+
+// PPUSTATUS $2002
+
+constexpr uint8_t PPU_OPEN_BUS_MASK = 0x1F;
+constexpr uint8_t SPRITE_OVERFLOW_MASK = 0x20;
+constexpr uint8_t SPRITE_0_HIT_MASK = 0x40;
+constexpr uint8_t VBLANK_STARTED_MASK = 0x80;
+
+// Other Masks
+constexpr uint16_t VRAM_ADDR_MASK = 0x3FFF;
 
 class Cartridge;
 
@@ -19,7 +51,50 @@ public:
     bool NMI();
 
 private:
+    uint8_t Read(uint16_t addr);
+    void Write(uint16_t addr, uint8_t data);
+
+    bool RenderingEnabled();
+    void IncrementVRAMAddr();
+    void CoarseXIncrement();
+    void YIncrement();
+
+// Other components
+private:
     Cartridge& cartridge;
+
+// Registers
+private:
+    struct
+    {
+        uint16_t v; // Current VRAM address
+        uint16_t t; // Temporary VRAM address
+        uint8_t x;  // Fine X scroll
+        bool w;     // Write toggle
+    } InternalRegisters;
+
+    struct
+    {
+        uint8_t PPUCTRL;    // $2000
+        uint8_t PPUMASK;    // $2001
+        uint8_t PPUSTATUS;  // $2002
+        uint8_t OAMADDR;    // $2003
+        uint8_t OAMDATA;    // $2004
+        uint8_t PPUSCROLL;  // $2005
+        uint8_t PPUADDR;    // $2006
+        uint8_t PPUDATA;    // $2007
+    } MemMappedRegisters;
+
+    uint8_t readBuffer;
+
+// Memory
+private:
+    std::array<uint8_t, 0x0100> OAM;
+
+// Frame state
+private:
+    uint16_t scanline;
+    uint16_t dot;
 };
 
 #endif
--- src/PPU.cpp
+++ src/PPU.cpp
@@ -1,4 +1,5 @@
 #include "../include/PPU.hpp"
+#include "../include/RegisterAddresses.hpp"
 #include <cstdint>
 
 PPU::PPU(Cartridge& cartridge) :
@@ -14,17 +15,194 @@ void PPU::Reset()
 
 uint8_t PPU::ReadReg(uint16_t addr)
 {
-    (void)addr;
-    return 0x00;
+    if (addr > 0x2007)
+    {
+        addr = 0x2000 + (addr % 0x08);
+    }
+
+    uint8_t returnData = 0x00;
+
+    switch (addr)
+    {
+        case PPUSTATUS_ADDR:
+            InternalRegisters.w = false;
+            returnData = MemMappedRegisters.PPUSTATUS;
+            MemMappedRegisters.PPUSTATUS &= 0x7F;
+            break;
+        case OAMDATA_ADDR:
+            returnData = OAM[MemMappedRegisters.OAMADDR];
+            break;
+        case PPUDATA_ADDR:
+            if (InternalRegisters.v < 0x3F00)
+            {
+                returnData = readBuffer;
+                readBuffer = Read(InternalRegisters.v & VRAM_ADDR_MASK);
+            }
+            else
+            {
+                returnData = Read(InternalRegisters.v & VRAM_ADDR_MASK);
+                readBuffer = Read(InternalRegisters.v - 0x1000);
+            }
+
+            if (RenderingEnabled() && scanline < 240)
+            {
+                CoarseXIncrement();
+                YIncrement();
+            }
+            else
+            {
+                IncrementVRAMAddr();
+            }
+            break;
+        default:
+            break;
+    }
+
+    return returnData;
 }
 
 void PPU::WriteReg(uint16_t addr, uint8_t data)
 {
-    (void)addr;
-    (void)data;
+    if (addr > 0x2007)
+    {
+        addr = 0x2000 + (addr % 0x08);
+    }
+
+    switch (addr)
+    {
+        case PPUCTRL_ADDR:
+            MemMappedRegisters.PPUCTRL = data;
+            InternalRegisters.t &= 0x73FF;
+            InternalRegisters.t |= ((data & 0x03) << 10);
+            break;
+        case PPUMASK_ADDR:
+            MemMappedRegisters.PPUMASK = data;
+            break;
+        case OAMADDR_ADDR:
+            MemMappedRegisters.OAMADDR = data;
+            break;
+        case OAMDATA_ADDR:
+            OAM[MemMappedRegisters.OAMADDR] = data;
+            ++MemMappedRegisters.OAMADDR;
+            break;
+        case PPUSCROLL_ADDR:
+            if (InternalRegisters.w)
+            {
+                InternalRegisters.t &= 0x0C1F;
+                InternalRegisters.t |= (((data & 0x07) << 12) | ((data & 0xF8) << 2));
+                InternalRegisters.w = false;
+            }
+            else
+            {
+                InternalRegisters.t &= 0x7FE0;
+                InternalRegisters.t |= ((data & 0xF8) >> 3);
+                InternalRegisters.x = (data & 0x07);
+                InternalRegisters.w = true;
+            }
+            break;
+        case PPUADDR_ADDR:
+            if (InternalRegisters.w)
+            {
+                InternalRegisters.t &= 0x7F00;
+                InternalRegisters.t |= data;
+                InternalRegisters.v = InternalRegisters.t;
+                InternalRegisters.w = false;
+            }
+            else
+            {
+                InternalRegisters.t &= 0x00FF;
+                InternalRegisters.t |= ((data & 0x3F) << 8);
+                InternalRegisters.w = true;
+            }
+            break;
+        case PPUDATA_ADDR:
+            Write(InternalRegisters.v & VRAM_ADDR_MASK, data);
+            IncrementVRAMAddr();
+            break;
+        default:
+            break;
+    }
 }
 
 bool PPU::NMI()
 {
     return false;
+}
+
+uint8_t PPU::Read(uint16_t addr)
+{
+    (void)addr;
+    return 0x00;
+}
+
+void PPU::Write(uint16_t addr, uint8_t data)
+{
+    (void)addr;
+    (void)data;
+}
+
+bool PPU::RenderingEnabled()
+{
+    constexpr uint8_t renderFlags = SHOW_BACKGROUND_MASK | SHOW_SPITES_MASK;
+    return (MemMappedRegisters.PPUMASK & renderFlags) != 0x00;
+}
+
+void PPU::IncrementVRAMAddr()
+{
+    InternalRegisters.v &= 0x7FFF;
+
+    if ((MemMappedRegisters.PPUCTRL & INCREMENT_VRAM_MASK) == INCREMENT_VRAM_MASK)
+    {
+        InternalRegisters.v += 0x20;
+    }
+    else
+    {
+        InternalRegisters.v += 0x01;
+    }
+}
+
+void PPU::CoarseXIncrement()
+{
+    InternalRegisters.v &= 0x7FFF;
+
+    if ((InternalRegisters.v & 0x001F) == 31)
+    {
+        InternalRegisters.v &= ~0x001F;
+        InternalRegisters.v ^= 0x0400;
+    }
+    else
+    {
+        ++InternalRegisters.v;
+    }
+}
+
+void PPU::YIncrement()
+{
+    InternalRegisters.v &= 0x7FFF;
+
+    if ((InternalRegisters.v & 0x7000) != 0x7000)
+    {
+        InternalRegisters.v += 0x1000;
+    }
+    else
+    {
+        InternalRegisters.v &= ~0x7000;
+        uint16_t y = (InternalRegisters.v & 0x03E0) >> 5;
+
+        if (y == 29)
+        {
+            y = 0;
+            InternalRegisters.v ^= 0x0800;
+        }
+        else if (y == 31)
+        {
+            y = 0;
+        }
+        else
+        {
+            ++y;
+        }
+
+        InternalRegisters.v = (InternalRegisters.v & ~0x03E0) | (y << 5);
+    }
 }
