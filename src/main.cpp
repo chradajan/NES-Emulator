3afe352cf0dc197111eaac43e3404da1e768f9cb
chradajan <chradajan@gmail.com>
2022-09-07 16:05:42 -0700

    Pass path to rom when starting emulator

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,23 @@
 #include <iostream>
 #include <string>
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
-    std::string rom_path = "../roms/";
-    std::string save_path = "../saves/";
-    std::string rom_extension = ".nes";
-    std::string save_extension = ".sav";
+    if (argc <= 1)
+    {
+        std::cout << "Failed to specify path to ROM." << std::endl;
+        return 0;
+    }
 
-    std::string game;
-    std::cin >> game;
+    std::string romPath = argv[1];
+    std::string fileNameWithExtension = romPath.substr(romPath.find_last_of("/\\") + 1);
+    std::string fileName = fileNameWithExtension.substr(0, fileNameWithExtension.find_last_of('.'));
+    std::string savePath = "../saves/" + fileName + ".sav";
 
     std::array<char, 256 * 240 * 3> frameBuffer;
 
-    NES nes(rom_path + game + rom_extension,
-            save_path + game + save_extension,
+    NES nes(romPath,
+            savePath,
             frameBuffer.data());
 
     GameWindow gameWindow(nes, frameBuffer.data());
